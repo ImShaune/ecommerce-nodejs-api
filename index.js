a8fcf28608d2dc282c14554cc75cf2458b4cbdad
ImShaune <nndesigner@outlook.es>
2022-03-29 23:21:21 -0300

    mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 
-app.listen(5500, () => {
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log('DBConnection Succesfull!'))
+.catch((err) => {
+    console.log(err)
+});
+
+
+app.listen(5000, () => {
     console.log('Backend is ready!')
 })
