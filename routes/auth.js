047fd0b10766be474fbe17fc8750f71fa91b7dac
ImShaune <nndesigner@outlook.es>
2022-03-31 18:30:43 -0300

    JWT implementation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //Register
 router.post('/register', async (req, res) => {
@@ -37,10 +38,18 @@ router.post('/login', async (req, res) => {
             password !==req.body.password && 
                 res.status(401).json('Wrong credentials!');
 
-            const { password, ...others } = user;
+                const accessToken = jwt.sign({
+                    id: user._id,
+                    isAdmin: user.isAdmin,
+                },
+                process.env.JWT_SEC,
+                {expiresIn:"3d"}
+                );
+
+            const { password, ...others } = user._doc;
 
             
-            res.status(200).json(user);
+            res.status(200).json(others, acessToken);
 
 
         }   catch(err){
